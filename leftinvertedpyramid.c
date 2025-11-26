b9c31e88de32497e1ea28d022f9db422f7bd8573
Manuel Rajesh <manuelrajesh39@gmail.com>
2025-11-26 17:04:07 +0530

    Add files via upload

---
+++ leftinvertedpyramid.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n=5;
+    for(int row=n;row>=1;row--){
+        for(int col=row;col>=1;col--){
+            printf("*");
+        }
+        printf("\n");
+
+
+}
+
+
+}

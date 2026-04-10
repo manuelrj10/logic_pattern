90a05ba0c471d6ebdd0083294429cab8b4599083
Manuel Rajesh <manuelrajesh39@gmail.com>
2026-04-10 22:15:43 +0530

    Add files via upload

---
+++ SolidSquare.java
@@ -0,0 +1,14 @@
+class SolidSquare{
+    public static void main(String[] args){
+        //row
+
+        
+        for(int i=0;i<5;i++){
+            //column
+            for(int j=0;j<5;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
